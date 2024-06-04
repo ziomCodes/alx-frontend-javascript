4d5cc8c8f8c48028100ffe5e7fc463802dcccd27
ziomCodes <ziomcode@gmail.com>
2024-06-04 07:16:00 +0000

    updated dm

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,17 @@
-export default function getStudentsByLocation(array, city, grad) {
-  return array
-    .filter((i) => i.location === city)
-    .map((student) => {
-      const gradeI = grad
-        .filter((i) => i.studentId === student.id)
-        .map((x) => x.grade)[0];
-      const grade = gradeI || 'N/A';
-      return { ...student, grade };
-    });
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const defaultGrade = { grade: 'N/A' };
+
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
 }
